1499a19512c1338ca4b24ac69287685734f967b6
Javier <javiergarcferrer@gmail.com>
2023-07-17 14:23:05 -0400

    ui update

--- dmenu/config.def.h
+++ dmenu/config.def.h
@@ -1,7 +1,8 @@
 /* See LICENSE file for copyright and license details. */
+
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
@@ -14,8 +15,8 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#ebdbb2", "#8ec07c" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
-static unsigned int lines      = 0;
-static unsigned int columns    = 0;
+static unsigned int lines      = 2;
+static unsigned int columns    = 4;
 
 /*
  * Characters not considered part of a word while deleting words
